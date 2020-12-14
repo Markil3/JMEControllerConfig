b35a66a89568f154004360a6efa503f29cca0633
Markil3 <>
2020-12-13 22:18:27 -0700

    Adds a basic app state class.

---
+++ src/main/java/markil3/controller/JoystickPreviewScreen.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2020 Markil 3. All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package markil3.controller;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.BaseAppState;
+import com.jme3.input.RawInputListener;
+import com.jme3.input.event.JoyAxisEvent;
+import com.jme3.input.event.JoyButtonEvent;
+import com.jme3.input.event.KeyInputEvent;
+import com.jme3.input.event.MouseButtonEvent;
+import com.jme3.input.event.MouseMotionEvent;
+import com.jme3.input.event.TouchEvent;
+
+/**
+ * @author Markil 3
+ */
+public class JoystickPreviewScreen extends BaseAppState
+        implements RawInputListener
+{
+
+    @Override
+    public void initialize(Application app)
+    {
+    }
+
+    @Override
+    protected void cleanup(Application app)
+    {
+    }
+
+    @Override
+    protected void onEnable()
+    {
+    }
+
+    @Override
+    protected void onDisable()
+    {
+    }
+
+    @Override
+    public void onJoyAxisEvent(JoyAxisEvent evt)
+    {
+    }
+
+    @Override
+    public void onJoyButtonEvent(JoyButtonEvent evt)
+    {
+    }
+
+    @Override
+    public void beginInput()
+    {
+    }
+
+    @Override
+    public void endInput()
+    {
+    }
+
+    @Override
+    public void onMouseMotionEvent(MouseMotionEvent evt)
+    {
+    }
+
+    @Override
+    public void onMouseButtonEvent(MouseButtonEvent evt)
+    {
+    }
+
+    @Override
+    public void onKeyEvent(KeyInputEvent evt)
+    {
+    }
+
+    @Override
+    public void onTouchEvent(TouchEvent evt)
+    {
+    }
+}
